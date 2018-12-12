48194474f8c222c271fd8393081860c85a82ab0c
jason <shicong.zhang@juniorchina.com>
2018-12-12 15:56:32 +0800

    test

--- 代码/dive-in-spring-boot/autoconfigure/src/main/java/com/imooc/diveinspringboot/configuration/HelloWorldConfiguration.java
+++ 代码/dive-in-spring-boot/autoconfigure/src/main/java/com/imooc/diveinspringboot/configuration/HelloWorldConfiguration.java
@@ -15,7 +15,7 @@ public class HelloWorldConfiguration {
 
     @Bean
     public String helloWorld() { // 方法名即 Bean 名称
-        return "Hello,World 2018";
+        return "Hello,World 2018---";
     }
 
 }
